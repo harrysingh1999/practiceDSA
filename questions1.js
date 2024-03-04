0267912d535e345229d02c9e772e28be864b7811
Harvinder Singh <hs541156@gmail.com>
2024-03-04 20:37:23 +0530

    final commit

--- questions1.js
+++ questions1.js
@@ -3,16 +3,17 @@
 const arr = [23, 232, 5634, 3223, 633, 234];
 
 const addValue = () => {
-  let targetIdx = 3;
+  let targetIdx = 1;
   let targetValue = 342;
 
-  for (let i = arr.length - 1; i >= targetIdx; i--) {
-    if (i > targetIdx) {
+  for (let i = arr.length-1; i >= targetIdx; i--) {
+    if (i >= targetIdx) {
       arr[i + 1] = arr[i];
-    } else if (i === targetIdx) {
+     if (i === targetIdx) {
       arr[i] = targetValue;
     }
   }
+}
   console.log(arr);
 };
 
